9974c02711f5591a419c5d9fadd46e4070dbbd02
andersonsimioni <anderson.simioni22@gmail.com>
2021-09-06 03:39:28 -0300

    First Commit

---
+++ Encryptor Core/Encryptor Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Encryptor_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
