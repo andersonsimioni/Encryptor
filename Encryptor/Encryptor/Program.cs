538fd3246d5db19c4c0593974dcecf9b9870ef3d
andersonsimioni <anderson.simioni22@gmail.com>
2021-09-06 03:44:28 -0300

    Correct Source

---
+++ Encryptor/Encryptor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Encryptor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
